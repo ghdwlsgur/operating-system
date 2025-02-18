2963bc588e7844b6b2e4be106532c8ade61b45c3
HongJinHyeok <vjinhyeokv@gmail.com>
2025-02-18 23:02:18 +0900

    C 표준 라이브러리 코드 작성
    - 메모리 조작 함수 (*memset, *memcpy)
    - 문자열 조작 함수 (*strcpy, strcmp)

--- common.c
+++ common.c
@@ -2,6 +2,70 @@
 
 void putchar(char ch);
 
+/**
+ * @brief 메모리 내용을 한 곳에서 다른 곳으로 복사 (src -> dst)
+ *
+ * @param dst 복사할 대상 위치
+ * @param src 복사할 원본 데이터 위치
+ * @param n 복사할 바이트 수
+ * @return void* 복사된 메모리의 시작 주소
+ */
+void *memcpy(void *dst, const void *src, size_t n) {
+  uint8_t *d = (uint8_t *)dst;             // 목적지 포인터
+  const uint8_t *s = (const uint8_t *)src; // 원본 포인터
+  while (n--)
+    *d++ = *s++; // 현재 위치 복사 후, s, d 포인터를 1씩 증가
+  return dst;
+}
+
+/**
+ * @brief 메모리의 특정 영역을 주어진 값으로 채움 (메모리 초기화)
+ *
+ * @param buf 값을 채울 메모리의 시작 주소
+ * @param c 채울 값
+ * @param n 채울 바이트 수
+ * @return void* 초기화된 메모리의 시작 주소
+ */
+void *memset(void *buf, char c, size_t n) {
+  uint8_t *p = (uint8_t *)buf;
+  while (n--)
+    *p++ = c; // 현재 위치에 c값을 채운 후, p 포인터를 1씩 증가
+  return buf;
+}
+
+/**
+ * @brief 문자열 복사
+ *
+ * @param dst 복사될 대상 문자열의 주소
+ * @param src 원본 문자열의 주소
+ * @return char* 복사된 문자열의 시작 주소
+ */
+char *strcpy(char *dst, const char *src) {
+  char *d = dst;
+  while (*src)
+    *d++ = *src++; // 현재 위치 복사 후, src, d 포인터를 1씩 증가
+  *d = '\0';
+  return dst;
+}
+
+/**
+ * @brief 두 문자열 비교
+ *
+ * @param s1 비교할 문자열 1
+ * @param s2 비교할 문자열 2
+ * @return int 두 문자열이 같으면 0, 다르면 0이 아닌 값
+ */
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && *s2) {
+    if (*s1 != *s2)
+      break;
+    s1++;
+    s2++;
+  }
+
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
 void printf(const char *fmt, ...) {
   va_list vargs;
   va_start(vargs, fmt);
--- common.h
+++ common.h
@@ -1,9 +1,39 @@
 #pragma once
 
+typedef int bool;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef uint32_t size_t;
+
+// 물리 메모리 주소를 나타내는 타입 (pysical memory address)
+typedef uint32_t paddr_t;
+// 가상 메모리 주소를 나타내는 타입 (virtual memory address)
+typedef uint32_t vaddr_t;
+
+#define true 1
+#define false 0
+#define NULL ((void *)0) // NULL을 void 포인터의 0으로 설정
+
+// value를 align의 배수로 맞춰 올림, 여기서 align은 2의 거듭제곱
+#define align_up(value, align) __builtin_align_up(value, align)
+// value가 align의 배수인지 확인
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+// 구조체 내에서 특정 멤버가 시작되는 위치(바이트 단위)를 반환
+#define offsetof(type, member) __builtin_offsetof(type, member)
+
 // 아래 매크로는 컴파일러의 내장 기능을 사용하여 가변 인자를 처리
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end __builtin_va_end
 #define va_arg __builtin_va_arg
 
+// 메모리 조작 함수들
+void *memset(void *buf, char c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+
+// 문자열 조작 함수들
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);
