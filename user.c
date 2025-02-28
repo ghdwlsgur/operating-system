1d500f6fdc7b90b2a30c97da33ab361f737e31c2
HongJinHyeok <vjinhyeokv@gmail.com>
2025-02-28 14:21:18 +0900

    시스템콜 함수 구현
    ecall 사용, 일반적으로 RISC-V에서는 a7에 시스템 콜 번호를 저장하지만 여기서는 a3 레지스터에 저장하도록 처리

--- user.c
+++ user.c
@@ -15,3 +15,33 @@ start(void) {
                        "call main \n"
                        "call exit \n" ::[stack_top] "r"(__stack_top));
 }
+
+/**
+ * @brief 시스템 콜을 호출
+ * 1. 함수 인자들을 RISC-V의 인자 레지스터에 로드
+ * 2. 시스템 콜 번호(sysno)를 a3 레지스터에 로드
+ * 3. ecall 명령어를 실행하여 운영체제 커널로 제어를 전달
+ * 4. 커널이 요청된 시스템 콜을 실행하고 결과를 a0 레지스터에 저장
+ * 5. 함수는 a0 레지스터의 값을 반환
+ *
+ * @param sysno 실행할 시스템 콜의 번호
+ * @param arg0 시스템 콜에 전달할 인자0
+ * @param arg1 시스템 콜에 전달할 인자1
+ * @param arg2 시스템 콜에 전달할 인자2
+ * @return int
+ */
+int syscall(int sysno, int arg0, int arg1, int arg2) {
+  // 레지스터 할당
+  register int a0 __asm__("a0") = arg0;
+  register int a1 __asm__("a1") = arg1;
+  register int a2 __asm__("a2") = arg2;
+  // 시스템 콜 번호를 저장
+  register int a3 __asm__("a3") = sysno;
+
+  __asm__ __volatile__("ecall" // 예외 핸들러 호출, 제어권을 커널로 넘김
+                       : "=r"(a0)
+                       : "r"(a0), "r"(a1), "r"(a2), "r"(a3)
+                       : "memory");
+
+  return a0;
+}
